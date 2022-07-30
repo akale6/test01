e7ea952798c886a61c062661f77bbebdd9971ac6
akale6 <110296351+akale6@users.noreply.github.com>
2022-07-30 23:17:50 +0800

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello{        // public class hello//表示hello是一个类，是一个public公有的类   Hello{}表示一个类的开始和结束
+	public static void main(String[]args){      //表示一个主方法 ，即程序的入口   main（）{}表示方法的开始和结束 
+		System.out.println("hello,word~");//表示输出 输出hello word 输出到屏幕上 分号表示语句结束
+	}
+} 
+	
